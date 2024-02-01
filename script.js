6b2783b8a7516fca7bc5843882d81a0298ddee72
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-01 18:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function stringChop(str, size) {
   let result = [];
+	 if (typeof str !== 'string' || typeof size !== 'number' || size <= 0) {
+        console.error('Invalid input. Please provide a valid string and a positive chunk length.');
+        return result;
+    }
+
     for(let i=0; i<str.length; i +=size){
       let chunk = str.slice(i,i+size);
         result.push(chunk);
@@ -10,6 +15,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
